89fe1612e2253f48baf87ac7b9bddb297ddc8917
atalaia <atalaia@apem.com.br>
2022-08-28 05:33:36 -0300

    second

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,18 @@
 import postsFromDB from "../data.json" assert {type: "json"};
 const postContainer = document.querySelector(".post-container");
 
+window.onload = function() {
+    buildAllPosts(postsFromDB);
+    filterButtons();
+};
+
 function buildAllPosts(json) {
     let innerHtmlPostsContainer = "";
     json.forEach(x => innerHtmlPostsContainer += buildPost(x));
     postContainer.innerHTML = innerHtmlPostsContainer;
 }
 function buildPost(data) {
-    return `<div class="post">
+    return `<div class="post ${data.group}">
     <img class="post-media" src=${data.picture}>
     <div class="post-texts">
     <div class="posts-header">
@@ -19,4 +24,46 @@ function buildPost(data) {
     </div>`;
 }
 
-window.onload(buildAllPosts(postsFromDB));
+// store preferences on url
+const url = new URL(window.location);
+
+const all = document.querySelector("#all");
+const group1 = document.querySelector("#group1");
+const group2 = document.querySelector("#group2");
+
+function filterButtons() {
+    if (url.searchParams.get("all") === "1") {
+        all.checked = true;
+        all.parentElement.style.background = "#aea"
+    }
+    if (url.searchParams.get("group1") === "1") {
+        group1.checked = true;
+        group1.parentElement.style.background = "#aea"
+    }
+    if (url.searchParams.get("group2") === "1") {
+        group2.checked = true;
+        group2.parentElement.style.background = "#aea"
+    }
+}
+
+// click filter buttons
+const filterMenu = document.querySelector(".filter-menu");
+
+filterMenu.addEventListener("click", (e) => {
+    const param = e.target.innerText;
+    if (!e.target.checked) {
+        url.searchParams.set(param, "1")
+    } else {
+        url.searchParams.set(param, "0")
+    }
+    
+    url.searchParams.get("all") === "1"
+        ? all.parentElement.style.background = "#aea"
+        : all.parentElement.style.background = "#fff";
+    url.searchParams.get("group1") === "1"
+        ? group1.parentElement.style.background = "#aea"
+        : group1.parentElement.style.background = "#fff";
+    url.searchParams.get("group2") === "1"
+        ? group2.parentElement.style.background = "#aea"
+        : group2.parentElement.style.background = "#fff";
+});
