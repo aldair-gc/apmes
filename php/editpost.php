446aa1e55d16ac2bc23c7dcd958116c9f77e5340
Aldair Garros Carvalho <98914031+aldair-gc@users.noreply.github.com>
2022-09-01 09:46:05 -0300

    test

--- php/editpost.php
+++ php/editpost.php
@@ -4,25 +4,17 @@ require_once('db.php');
 
 // check if the fields exist
 if (!isset($_POST['groupname'], $_POST['title'], $_POST['content'], $_POST['id'])) {
-    exit('Data missing.');
+    header('Location: /feed_editor.php?msg=5');
 }
 
 // check if the fields are not empty
 if (empty($_POST['groupname']) || empty($_POST['title']) || empty($_POST['content']) || empty($_POST['id'])) {
-    exit('The fields must be fulfilled.');
+    header('Location: /feed_editor.php?msg=6');
 }
 
 // handle submitted file
-if (!isset($_FILES['file']) || empty($_FILES['file'])) {
-    // save the new post in the database
-    if ($stmt = $conn->prepare('UPDATE posts SET groupname=?, title=?, content=? WHERE id=?')) {
-        $stmt->bind_param('sssi', $_POST['groupname'], $_POST['title'], $_POST['content'], $_POST['id']);
-        $stmt->execute();
-        header('Location: /feed_editor.php');
-    } else {
-        header('Location: /feed_editor.php?msg=3');
-    }
-} else {
+if (isset($_FILES['file']) && !empty($_FILES['file'])) {
+    // prepare file's name and save it
     $uploaddir = '../uploads/';
     $origfilename = $_FILES['file']['name'];
     $extension = end(explode('.', $origfilename));
@@ -36,7 +28,16 @@ if (!isset($_FILES['file']) || empty($_FILES['file'])) {
     if ($stmt = $conn->prepare('UPDATE posts SET groupname=?, title=?, content=? file=? WHERE id=?')) {
         $stmt->bind_param('ssssi', $_POST['groupname'], $_POST['title'], $_POST['content'], $loadpath, $_POST['id']);
         $stmt->execute();
-        header('Location: /feed_editor.php');
+        header('Location: /feed_editor.php?msg=10');
+    } else {
+        header('Location: /feed_editor.php?msg=3');
+    }
+} else {
+    // save the new post in the database
+    if ($stmt = $conn->prepare('UPDATE posts SET groupname=?, title=?, content=? WHERE id=?')) {
+        $stmt->bind_param('sssi', $_POST['groupname'], $_POST['title'], $_POST['content'], $_POST['id']);
+        $stmt->execute();
+        header('Location: /feed_editor.php?msg=10');
     } else {
         header('Location: /feed_editor.php?msg=3');
     }
