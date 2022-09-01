7dad7366b55c04e51c577301ae73888aecce7886
Aldair Garros Carvalho <98914031+aldair-gc@users.noreply.github.com>
2022-09-01 01:43:00 -0300

    teste

--- post_editor.php
+++ post_editor.php
@@ -28,8 +28,7 @@ $post = mysqli_query($conn, "SELECT * FROM posts WHERE id=$urlid");
 
                         <?php while ($row = mysqli_fetch_array($groups)) { ?>
                             <div class="radio-option">
-                                <input type="radio" name="groupname" id="<?php echo $row['groupname']; ?>" value="<?php echo $row['groupname']; ?>"
-                                    <?php echo (($row['groupname'] == $post['groupname']) ? ' checked' : '') ?>>
+                                <input type="radio" name="groupname" id="<?php echo $row['groupname']; ?>" value="<?php echo $row['groupname']; ?>">
                                 <label for="<?php echo $row['groupname']; ?>"><?php echo $row['groupname']; ?></label>
                             </div>
                         <?php } ?>
