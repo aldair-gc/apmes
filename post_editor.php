7d6da1d013871c51b83a10065ba52cb024af40cc
Aldair Garros Carvalho <98914031+aldair-gc@users.noreply.github.com>
2022-09-01 01:51:40 -0300

    test

--- post_editor.php
+++ post_editor.php
@@ -5,6 +5,7 @@ require('php/fetch_groups.php');
 require('components/header.php');
 $urlid = htmlspecialchars($_GET["id"]);
 $post = mysqli_query($conn, "SELECT * FROM posts WHERE id=$urlid");
+echo $post
 ?>
 
 <main>
