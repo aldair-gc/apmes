cffc1a8490ba4e267da9c9740bfc84725c39c677
Aldair Garros <aldair.gc@outlook.com>
2022-09-02 20:57:06 -0300

    msg box map set

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,4 @@
-// import postsFromDB from "../data.json" assert {type: "json"};
-// import buildAllPostsEdit from "./build-posts-edit.js";
-// import buildAllPosts from "./build-posts.js";
-
-// const postContainer = document.querySelector(".post-container");
-
 const url = new URL(window.location);
-
-
-// const group1 = document.querySelector("#group1");
-// const group2 = document.querySelector("#group2");
-// const paramAll = url.searchParams.get("all")
-// const paramGroup1 = url.searchParams.get("group1")
-// const paramGroup2 = url.searchParams.get("group2")
-
-// window.onload = function () {
-//     // postContainer.innerHTML = buildAllPostsEdit(postsFromDB);
-//     if (url.searchParams.get("all") === "1") {
-//         all.classList.add("active")
-//     }
-//     if (url.searchParams.get("group1") === "1") {
-//         group1.classList.add("active")
-//     }
-//     if (url.searchParams.get("group2") === "1") {
-//         group2.classList.add("active")
-//     };
-// }
 
 
 // click filter buttons
@@ -34,7 +8,7 @@ if (filters) {
         const filterButton = e.target.innerText; // get button's text
         const allPosts = document.querySelectorAll(".post"); // select all these posts
         const all = document.querySelector("#all");
-
+        
         const groups = document.querySelectorAll(".groupname");
         const groupObjects = [all];
         groups.forEach((value) => groupObjects.push(value));
@@ -69,35 +43,38 @@ if (filters) {
 };
 
 
-// Message box
-const msgContainer = document.querySelector(".msg-container");
-const allMsg = document.querySelectorAll(".msg");
-// Add click effect to remove messages
-allMsg.forEach(msg => {
-    msg.addEventListener("click", (e) => {
-        msg.classList.remove("show");
-        console.log("click");
-    });
-});
 // Make the messages from url codes
 codeList = new Map([
-    [1, "Wrong password!"],
-    [2, "Email not registered!"],
-    [3, "Error processing register request!"],
-    [4, "Could not prepare statement!"],
-    [5, "Missing data!"],
-    [6, "All field must be fulfilled!"],
-    [7, "Invalid email!"],
-    [8, "Password must be between 5 and 20 characters long!"],
-    [9, "Email already registered!"],
-    [10, "Post edited successfully!"],
-    [11, "Post deleted successfully!"],
+    ["1", "Wrong password!"],
+    ["2", "Email not registered!"],
+    ["3", "Error processing register request!"],
+    ["4", "Could not prepare statement!"],
+    ["5", "Missing data!"],
+    ["6", "All field must be fulfilled!"],
+    ["7", "Invalid email!"],
+    ["8", "Password must be between 5 and 20 characters long!"],
+    ["9", "Email already registered!"],
+    ["10",  "Post edited successfully!"],
+    ["11",  "Post deleted successfully!"],
 ]);
+
 const msgCodesInUrl = url.searchParams.getAll("msg");
+const msgContainer = document.querySelector(".msg-container");
+
 window.onload = function () {
+    // Message box
     let html = ``;
-    msgCodesInUrl.forEach((code) => {
+    msgCodesInUrl.map((code) => {
         html += `<div class="msg show">${codeList.get(code)}</div>`
-    })
-    //continue...
-}
+    });
+    msgContainer.innerHTML = html;
+    const allMsg = document.querySelectorAll(".msg");
+
+    // Add click effect to remove messages
+    allMsg.forEach(msg => {
+        msg.addEventListener("click", (e) => {
+            msg.classList.remove("show");
+            console.log("click");
+        });
+    });
+};
