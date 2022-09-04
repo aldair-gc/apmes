139255fdc3b22d30f43e0c1bf5b15a80900c7bb7
Aldair Garros Carvalho <98914031+aldair-gc@users.noreply.github.com>
2022-09-04 19:09:46 -0300

    test

--- components/header.php
+++ components/header.php
@@ -7,7 +7,10 @@
     <title>Apostolic & Prophetic Ministries El Shaddai</title>
     <link rel="stylesheet" type="text/css" href="/public/css/style.css?v=<?php echo microtime() ?>">
     <script src="https://kit.fontawesome.com/2cc3b4dcd6.js" crossorigin="anonymous"></script>
-</head>
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,400;0,700;1,400;1,700&family=Open+Sans:ital,wght@0,300;0,400;1,300;1,400&family=Roboto:ital,wght@0,100;0,300;0,400;1,100;1,300;1,400&display=swap" rel="stylesheet"></head>
 
 <body>
 <nav>
