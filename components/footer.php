c6bb3ac000c3cafc1fffabf97c778d86dd1ead2f
Aldair Garros Carvalho <98914031+aldair-gc@users.noreply.github.com>
2022-09-05 11:01:59 -0300

    footer

--- components/footer.php
+++ components/footer.php
@@ -1,14 +1,37 @@
 <footer>
         <div class="footer-container">
             <div class="footer-col">
-                footer1
-                <a href="/contact.php">Contact</a>
+                <h3>Site's map</h3>
+                <a href="/contact.php">Home</a>
+                <a href="/contact.php">The Church</a>
+                <a href="/contact.php">Our Family</a>
+                <a href="/contact.php">Feed</a>
+                <a href="/contact.php">Prayer Requests</a>
+                <a href="/contact.php">Missions</a>
+                <a href="/contact.php">Agenda</a>
+                <a href="/contact.php">Pictures</a>
+                <a href="/contact.php">Videos</a>
+                <a href="/contact.php">Find Us</a>
             </div>
             <div class="footer-col">
-                footer2
+                <h3>Recomendations</h3>
+                <a href="http://">Website A</a>
+                <a href="http://">Website B</a>
+                <a href="http://">Website C</a>
+                <a href="http://">Website D</a>
+                <a href="http://">Website E</a>
+                <a href="http://">Website F</a>
+                <a href="http://">Website G</a>
+                <a href="http://">Website H</a>
             </div>
             <div class="footer-col">
+                <h3>Internal</h3>
                 <a href="/login.php">Management</a>
+                <a href="/login.php">Optional 1</a>
+                <a href="/login.php">Optional 2</a>
+                <a href="/login.php">Optional 3</a>
+                <a href="/login.php">Optional 4</a>
+                <a href="/login.php">Optional 5</a>
             </div>
         </div>
     </footer>
