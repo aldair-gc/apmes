2a5a3e976289eb89a587e0c43ce7cdc2201fd026
Aldair Garros Carvalho <98914031+aldair-gc@users.noreply.github.com>
2022-09-06 11:54:41 -0300

    session fix

--- php/buildposts.php
+++ php/buildposts.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 if (isset($_SESSION['loggedin'])) { ?>
 
     <?php while ($row = mysqli_fetch_array($posts)) { ?>
--- php/feed_right_menu.php
+++ php/feed_right_menu.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 if (isset($_SESSION['loggedin'])) { ?>
 
     <ul id="filter-right-menu">
