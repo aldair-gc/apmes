c575bbe728daca43897fe6b12e1635cee9697bac
Aldair Garros Carvalho <98914031+aldair-gc@users.noreply.github.com>
2022-09-06 15:25:39 -0300

    test

--- php/newgroup.php
+++ php/newgroup.php
@@ -13,7 +13,7 @@ if (empty($_POST['groupname'])) {
 }
 
 // check if the characters are valid
-if (preg_match('/\S{1,20}/', $_POST['groupname']) == 0) {
+if (preg_match('/\s\g/', $_POST['groupname']) > 0) {
     header('Location: /groups.php?msg=eu');
 }
 
