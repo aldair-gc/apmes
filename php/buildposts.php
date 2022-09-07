5d9f8167c1653be6ce6399e435ab21bb85bfcaa4
Aldair Garros <aldair.gc@outlook.com>
2022-09-07 14:53:36 -0300

    post fix

--- php/buildposts.php
+++ php/buildposts.php
@@ -6,13 +6,13 @@
 
         <?php require('post.php') ?>
 
-            <div class="post-control">
-                <a class="midbutton" href="<?php echo '/post_editor.php?id=' . $row['id']; ?>">
-                    <i class="fa-solid fa-pen-to-square"></i>edit
-                </a>
-                <a class="midbutton font-red" href="<?php echo '/post_delete.php?id=' . $row['id']; ?>">
-                    <i class="fa-solid fa-eraser"></i>delete
-                </a>
+        <div class="post-control">
+            <a class="midbutton" href="<?php echo '/post_editor.php?id=' . $row['id']; ?>">
+                <i class="fa-solid fa-pen-to-square"></i>edit
+            </a>
+            <a class="midbutton font-red" href="<?php echo '/post_delete.php?id=' . $row['id']; ?>">
+                <i class="fa-solid fa-eraser"></i>delete
+            </a>
             </div>
         </div>
 
@@ -23,6 +23,7 @@
     <?php while ($row = mysqli_fetch_array($posts)) { ?>
         
         <?php require('post.php') ?>
+        </div>
 
     <?php } ?>
 
--- php/post.php
+++ php/post.php
@@ -1,36 +1,34 @@
-            <div class="post <?php echo $row['groupname']; ?>">
+<div class="post <?php echo $row['groupname']; ?>">
 
-                <?php if ($row['youtubeurl'] !== '') { ?>
+    <?php if ($row['youtubeurl'] !== '') { ?>
 
-                    <div class="post-media">
-                        <iframe src="https://www.youtube.com/embed/<?php echo $row['youtubeurl'] ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
+        <div class="post-media">
+            <iframe src="https://www.youtube.com/embed/<?php echo $row['youtubeurl'] ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
 
-                <?php } else { ?>
-                    <?php $filechecked = ($row['file'] === '') ? '/public/images/logo3DPaper_1200.jpg' : $row['file'] ?>
-                    <?php $exploded = explode('.', $filechecked); $ext = strtolower(end($exploded)); ?>
-                    <?php if ($ext === 'jpg' || $ext === 'jpeg' || $ext === 'gif' || $ext === 'png' || $ext === 'heic') { ?>
+    <?php } else { ?>
+        <?php $filechecked = ($row['file'] === '') ? '/public/images/logo3DPaper_1200.jpg' : $row['file'] ?>
+        <?php $exploded = explode('.', $filechecked); $ext = strtolower(end($exploded)); ?>
+        <?php if ($ext === 'jpg' || $ext === 'jpeg' || $ext === 'gif' || $ext === 'png' || $ext === 'heic') { ?>
 
-                        <img class="post-media" src="<?php echo $filechecked; ?>"></img>
+            <img class="post-media" src="<?php echo $filechecked; ?>"></img>
 
-                    <?php } elseif ($ext === 'mov' || $ext === 'mpg' || $ext === 'mpeg' || $ext === 'avi' || $ext === 'wmv' ||
-                    $ext === 'ogg' || $ext === 'mp4' || $ext === 'webm') { ?>
+        <?php } elseif ($ext === 'mov' || $ext === 'mpg' || $ext === 'mpeg' || $ext === 'avi' || $ext === 'wmv' ||
+        $ext === 'ogg' || $ext === 'mp4' || $ext === 'webm') { ?>
 
-                        <video class="post-media" controls><source src="<?php echo $filechecked; ?>">Your browser does not support the video tag.</video>
+            <video class="post-media" controls><source src="<?php echo $filechecked; ?>">Your browser does not support the video tag.</video>
 
-                    <?php } elseif ($ext === 'mid' || $ext === 'midi' || $ext === 'wma' || $ext === 'aac' || $ext === 'wav' ||
-                    $ext === 'mp3') { ?>
+        <?php } elseif ($ext === 'mid' || $ext === 'midi' || $ext === 'wma' || $ext === 'aac' || $ext === 'wav' ||
+        $ext === 'mp3') { ?>
 
-                        <audio class="post-media" controls><source src="<?php echo $filechecked; ?>">Your browser does not support the audio tag.</audio>
-                    
-                    <?php }; }; ?>
+            <audio class="post-media" controls><source src="<?php echo $filechecked; ?>">Your browser does not support the audio tag.</audio>
+        
+        <?php }; }; ?>
 
-                <div class="post-texts">
-                    <div class="posts-header">
-                        <div class="posts-title"><?php echo $row['title']; ?></div>
-                        <div class="posts-date"><?php echo $row['date']; ?></div>
-                    </div>
-                    <div class="posts-content"><?php echo $row['content']; ?></div>
-                </div>
-
-            </div>
+    <div class="post-texts">
+        <div class="posts-header">
+            <div class="posts-title"><?php echo $row['title']; ?></div>
+            <div class="posts-date"><?php echo $row['date']; ?></div>
+        </div>
+        <div class="posts-content"><?php echo $row['content']; ?></div>
+    </div>
